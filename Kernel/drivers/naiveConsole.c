7053a4f58dc3d4b856d151468c1c1b23de43d26c
Matías Della Torre <72523405+matidellatorre@users.noreply.github.com>
2022-05-26 19:05:20 -0300

    Split screen implementation (work in progress)

--- Kernel/drivers/naiveConsole.c
+++ Kernel/drivers/naiveConsole.c
@@ -3,12 +3,13 @@
 static uint32_t uintToBase(uint64_t value, char * buffer, uint32_t base);
 
 static char buffer[64] = { '0' };
-static uint8_t * const video = (uint8_t*)0xB8000;
-static uint8_t * currentVideo = (uint8_t*)0xB8000;
-//static uint8_t * currentVideoR = video + width + 2; //posicion inicial de la division de pantalla (el + 2 es para que este en la mitad mas 1 a la der)
-//static uint8_t * currentVideoL = video;   //posicion inicial izq de la division de pantalla
 static const uint32_t width = 80;
 static const uint32_t height = 25 ;
+static uint8_t * const video = (uint8_t*)0xB8000;
+static uint8_t * currentVideo = (uint8_t*)0xB8000;
+static uint8_t * currentVideoL = (uint8_t *)0xB8000; //posicion inicial de la division de pantalla (el + 2 es para que este en la mitad mas 1 a la der)
+static uint8_t * currentVideoR = (uint8_t *)0xB8000;   //posicion inicial izq de la division de pantalla
+static int split = 0;
 
 void ncPrint(const char * string)
 {
@@ -16,6 +17,14 @@ void ncPrint(const char * string)
 
 	for (i = 0; string[i] != 0; i++)
 		ncPrintChar(string[i]);
+}
+
+void ncSplit(){
+	ncClear();
+	for(int i = 0; i < height; i++){
+		ncPrintCharAt('|',width/2,i);
+	}
+	split = 1;
 }
 
 void ncPrintChar(char character)
@@ -31,6 +40,47 @@ void ncPrintChar(char character)
 
 	*currentVideo = character;
 	currentVideo += 2;
+}
+
+void ncPrintCharL(char character)
+{
+	if(currentVideoL==(video+(width*height)*2)){
+		ncMoveLines();
+	}
+
+	if(character == '\n') {
+		ncNewlineL();
+		return;
+	}
+
+	if (currentVideoL!=video && (video-currentVideoL)%width==0){
+		// si llego al final del renglon, me salteo la mitad derecha y sigo escribiendo abajo
+		currentVideoL+=width;
+	}
+	
+
+	*currentVideoL = character;
+	currentVideoL += 2;
+}
+
+void ncPrintCharR(char character)
+{
+
+	if(currentVideoR==(video+(width*height)*2)){
+		ncMoveLinesR();
+	}
+
+	if ((video-currentVideoR)%(width*2)==0){
+		currentVideoR+=width+2;
+	}
+
+	if(character == '\n') {
+		ncNewlineR();
+		return;
+	}
+
+	*currentVideoR = character;
+	currentVideoR += 2;
 }
 
 void ncDeleteChar(){
@@ -51,6 +101,24 @@ void ncMoveLines(){
 	currentVideo=currentVideo-(width*2);
 }
 
+void ncMoveLinesR(){
+	for(int i=80;i<height*width*2;){
+		if (i%(width*2)==0){
+			i+=width;
+		}
+		if (i>=(height-1)*width*2){
+			video[i]=' ';
+			i+=2;
+		} else {
+			video[i]=video[i+width*2];
+			i++;
+		}
+	
+		
+	}
+	currentVideoR=currentVideoR-(width);
+}
+
 void ncPrintCharAt(char character, uint32_t x, uint32_t y){
 	// No tengo en cuenta currentVideo, simplemente inserto en char en el punto(x,y)
 	video[(width*y+x)*2]=character;
@@ -64,6 +132,50 @@ void ncNewline()
 	}
 	while((uint64_t)(currentVideo - video) % (width * 2) != 0);
 }
+
+void ncNewlineL()
+{
+	do
+	{
+		ncPrintCharL(' ');
+	}
+	while((uint64_t)(currentVideoL - video) % (width) != 0);
+}
+
+void ncNewlineR()
+{
+	do
+	{
+		ncPrintCharR(' ');
+	}
+	while((uint64_t)(currentVideoR - video) % (width * 2 * 2) != 0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 void ncPrintDec(uint64_t value)
 {
--- Kernel/include/naiveConsole.h
+++ Kernel/include/naiveConsole.h
@@ -13,6 +13,8 @@ void ncPrintHex(uint64_t value);
 void ncPrintBin(uint64_t value);
 void ncPrintBase(uint64_t value, uint32_t base);
 void ncClear();
-
+void ncPrintCharR(char character);
+void ncPrintCharL(char character);
+void ncSplit();
 
 #endif
